44e0f5c297c5fe5fbf79b63423b3b96f0959b595
reinaldo <reinaldo.aguilar@fundacion-jala.org>
2017-07-11 08:59:13 -0400

    This practitice with operator and convert second

---
+++ ReinaldoAguilar/Task2/Trying.rb
@@ -0,0 +1,29 @@
+
+=begin
+OK 1.- Take your previous script and change calculations for variables.
+OK 2.- Add some line in the middle of your script that is referring to a variable that doesn’t exist. Run the file and please explain the error displayed. 
+3.- Create a new script adding at least one case for each one of the arithmetic, comparison, assignment and logical operators, print them and also print the result obtained.
+
+=end
+a = 25 + 30 / 6
+b = 100 - 25 * 3 % 4
+c = 3 + 2
+d = 5 - 7
+
+puts "Bananas: #{a}" 
+puts "apples : #{b}"
+
+puts "Now I will count the potato"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts "Is it true that 3+2<5-7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3+2? it is #{c}"
+
+puts "What is 5-7? It is #{doesn’tExit}"
+
+hc = "hola"
+bc = 2
+
