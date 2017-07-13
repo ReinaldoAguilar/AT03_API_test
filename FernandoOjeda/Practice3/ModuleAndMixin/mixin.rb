8261b147609d69dfa0350a9a2ed4d80f616364d3
Administrator <fernando.ojeda@fundacion-jala.org>
2017-07-13 18:47:23 +0000

    Practice3

---
+++ FernandoOjeda/Practice3/ModuleAndMixin/mixin.rb
@@ -0,0 +1,26 @@
+
+module Greets
+
+  def Greets.greetOne()
+    puts "Hi friends"
+  end
+  def Greets.greetTwo()
+    puts "good night"
+  end
+  def Greets.gretThree()
+    puts "good afternoon"
+  end
+end
+
+module Bye
+
+  def Bye.byeOne()
+    puts "good bye"
+  end
+  def Bye.byeTwo()
+    puts "see you later"
+  end
+  def Bye.byeThree()
+    puts "take care"
+  end
+end
